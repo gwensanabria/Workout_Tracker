a0204270898a079c956ec6bf107f261521f40bc9
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-08-22 17:30:32 -0500

    create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const apiR = require('./routes/apiRoutes.js')
+const htmlR = require('./routes/htmlRoutes.js')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// ROUTES
+app.use(require('./routes/apiRoutes'))
+app.use(require('./routes/htmlRoutes'))
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
