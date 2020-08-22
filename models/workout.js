d1c1c92d6b8ee375ae4f6f0810659e9ef8b9883c
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-08-22 17:43:35 -0500

    workout schema

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,50 @@
 const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    workouts: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter the type of workout."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter the name of the workout."
+            },
+            duration: {
+                type: Number,
+                required: "Enter workout duration in minutes."
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+})
+
+workoutSchema.virtual('totalDuration').get(function() {
+    return this.workouts.reduce((total, workouts) => {
+        return total + workouts.duration
+    }, 0)
+})
+
+const Workout = mongoose.model("Workout", workoutSchema)
+
+module.exports = Workout
